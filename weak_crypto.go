2c2a682bdb3bdf21ff82812b67533a18acff5599
siddhant-deepsource <siddhant@deepsource.io>
2020-11-10 18:26:17 +0530

    Added RSA code

--- weak_crypto.go
+++ weak_crypto.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"crypto/md5"
+	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
 	"os"
 )
@@ -10,4 +12,13 @@ func makeMD5Hash() {
 	for _, arg := range os.Args {
 		fmt.Printf("%x - %s\n", md5.Sum([]byte(arg)), arg)
 	}
+}
+
+func generateRSAKey() {
+	//Generate Private Key
+	pvk, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(pvk)
 }
